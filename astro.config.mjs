ced533414eefed33a2d8000d44f1e130bb3dea03
Thorsten Schminkel <thorsten@schminkel.de>
2022-08-22 19:41:33 +0200

    add react component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig(
-  {
-    site: 'https://schminkel.github.io',
-    base: '/astrojs-github-pages'
-  });
+export default defineConfig({
+  site: 'https://schminkel.github.io',
+  base: '/astrojs-github-pages',
+  
+  integrations: [
+    react()
+  ]
+});
---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+
+const ContentContainer = () => {
+  return (
+    <>
+      <h1>TEST</h1>
+    </>
+      );
+};
+
+export default ContentContainer;
